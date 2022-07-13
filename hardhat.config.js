da64972eecabed04c336bbf9610b50428200a60b
Aakash <vermaaakash1000@gmail.com>
2022-07-13 17:02:22 +0530

    Goerli network added in config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const Goerli_RPC_URL = process.env.Goerli_RPC_URL;
+const Goerli_key = process.env.Goerli_key;
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: Goerli_RPC_URL,
+      accounts: [Goerli_key],
+      chainId: 5,
+    },
+  },
   solidity: "0.8.7",
 };
