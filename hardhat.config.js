183f7b8de37d90ce80dcef79918031b85811f5e4
Aakash <vermaaakash1000@gmail.com>
2022-07-13 19:05:28 +0530

    Etherscan api key added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const Goerli_RPC_URL = process.env.Goerli_RPC_URL;
 const Goerli_key = process.env.Goerli_key;
+const Ether_api = process.env.Ether_api;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -13,6 +15,9 @@ module.exports = {
       accounts: [Goerli_key],
       chainId: 5,
     },
+  },
+  etherscan: {
+    apiKey: Ether_api,
   },
   solidity: "0.8.7",
 };
