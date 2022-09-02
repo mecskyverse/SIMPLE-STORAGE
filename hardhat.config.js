22e8e826dec85df9d62e160bbd5ae18910792008
Aakash <vermaaakash1000@gmail.com>
2022-09-02 13:41:50 +0530

    rinkeby Network added

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,6 +11,11 @@ const Goerli_RPC_URL = process.env.Goerli_RPC_URL;
 const Goerli_key = process.env.Goerli_key;
 const Ether_api = process.env.Ether_api;
 const Coinmarketapi_KEY = process.env.coinmarketapi;
+
+const RINKEBY_RPC_URL = process.env.rinkebyRpcUrl;
+const PRIVATE_KEY = process.env.rinkebyKey;
+const ETHERSCAN_API_KEY = process.env.Ether_api;
+
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
@@ -18,6 +23,12 @@ module.exports = {
       url: Goerli_RPC_URL,
       accounts: [Goerli_key],
       chainId: 5,
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+      blockConfirmations: 6,
     },
     localhost: {
       url: "http://127.0.0.1:8545/",
